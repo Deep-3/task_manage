7a97ef9fe81ecd8089220028df9957cd69569da2
Deep-3 <109722431+Deep-3@users.noreply.github.com>
2025-01-12 18:32:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 //routes
 app.use('/ap',helloriutes);
-app.use('/users', userRoutes);
+app.use('/uses', userRoutes);
 app.use('/auth', loginRoutes);
 app.use('/event',eventRoutes);
 app.use('/ticket-booking', ticketBookingRoutes);
