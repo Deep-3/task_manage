b52d3d41400ae7168240666c60b59dbf3939708e
Deep-3 <109722431+Deep-3@users.noreply.github.com>
2025-01-13 10:05:20 +0530

    Update reportroutes.js

--- routes/reportroutes.js
+++ routes/reportroutes.js
@@ -5,7 +5,7 @@ const {authenticate,authorize}=require('../middleware/authmiddleware');
 
 router.get('/classreport/:pdf',authenticate,authorize(['admin','superadmin']),reportcontroller.generateEventReport)
 
-router.get('/customerreport/:pdf',authenticate,authorize(['admin','superadmin']),reportcontroller.generateCustomerReport)
+router.get('/customerr/:pdf',authenticate,authorize(['admin','superadmin']),reportcontroller.generateCustomerReport)
 
 router.get('/revenuereport/:pdf',authenticate,authorize(['admin','superadmin']),reportcontroller.generateRevenueReport)
 module.exports=router;
