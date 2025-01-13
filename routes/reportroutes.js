f683b60b3c35bc9f0d677e265af85f1461131082
Deep-3 <deepkalathiya03@gmail.com>
2025-01-13 10:02:42 +0530

    change in routes

--- routes/reportroutes.js
+++ routes/reportroutes.js
@@ -4,11 +4,8 @@ const router = express.Router();
 const {authenticate,authorize}=require('../middleware/authmiddleware');
 
 router.get('/classreport/:pdf',authenticate,authorize(['admin','superadmin']),reportcontroller.generateEventReport)
-router.get('/classreport/:csv',authenticate,authorize(['admin','superadmin']),reportcontroller.generateEventReport)
 
 router.get('/customerreport/:pdf',authenticate,authorize(['admin','superadmin']),reportcontroller.generateCustomerReport)
-router.get('/customerreport/:csv',authenticate,authorize(['admin','superadmin']),reportcontroller.generateCustomerReport)
 
 router.get('/revenuereport/:pdf',authenticate,authorize(['admin','superadmin']),reportcontroller.generateRevenueReport)
-router.get('/revenuereport/:csv',authenticate,authorize(['admin','superadmin']),reportcontroller.generateRevenueReport)
 module.exports=router;
